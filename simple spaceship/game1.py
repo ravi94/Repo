834d1ba9011ff3915604318f4b89a52bf948defa
Ravi Bhushan <ravibhushan94@gmail.com>
2013-04-12 18:40:50 +0530

    spped of ship changed

--- simple spaceship/game1.py
+++ simple spaceship/game1.py
@@ -10,10 +10,7 @@ e="explosion.png"
 
 import pygame,time,random,math
 from pygame.locals import *
-"""
-import serial
-ser = serial.Serial('/dev/ttyUSB0',9600)
-ser.flushInput()"""
+
 # variable declaration
 Ship=ship
 shots=[]
@@ -28,8 +25,8 @@ done=True ; done2=True
 ae =0 ; sh_e=0
 score=0
 level=0 ; left=0
-astr_s=201 ; t=200
-
+astr_s=201
+t=200
 pygame.init()
 screen=pygame.display.set_mode((800,600),0,32)
 background=pygame.image.load(bg).convert()
@@ -51,45 +48,15 @@ text4 = font1.render("Press any key to Quit...", 1, (255, 0, 0))
 def dist((e,f),(g,h)):
         return math.sqrt( (e-g)**2 + (f-h)**2)
 
-def cut(s):
-        if s.endswith('\n'):
-                return s[1:-1]
-        else:
-                return s[1:]
-
 
         
 
 while done :
- 
-        u="  ";
-        while(not(u[0]=='$')):
-                u=ser.readline()
-        y_axis=int(cut(u))      
-        while(not(u[0]=='#')):
-                u=ser.readline()        
-        x_axis=int(cut(u))
-        if (y_axis<90 and y_axis>-22) :
-                        #if event.key==K_UP:
-                                speed[1]=-250
-        elif (y_axis<255 and y_axis>130) :
-                        #if event.key==K_DOWN:   
-                                speed[1]=+250
-        else:
-                speed[1]=+0
-        if (x_axis<255 and x_axis>140) :
-                        #if event.key==K_UP:
-                                speed[0]=-250
-        elif (x_axis<114 and x_axis>0) :
-                        #if event.key==K_DOWN:   
-                                speed[0]=+250
-        else:
-                speed[0]=+0     
         
         text1 = font.render("Score : "+str(score), 1, (255, 0, 0))
         text2 = font.render("Level : "+str(level), 1, (255, 0, 0))
         text5 = font.render("Unshot Asteroid : "+str(left), 1, (255, 0, 0))
-        
+
 # defining Clock        
         ms=clock.tick()
         sec=ms/1000.0
@@ -97,12 +64,10 @@ while done :
         
 # score and level check        
         if score == 10:
-                astr_s+=(0.5*astr_s)
+                astr_s+=100
                 score=0
                 level+=1
                 t+=30
-                
-           
         spaceship=pygame.image.load(Ship).convert_alpha()
         
 
@@ -111,21 +76,15 @@ while done :
                 if event.type==QUIT:
                         done=False
                         done2=False
-                
                 if event.type==KEYDOWN:
-                        #if (mat<94 and mat>0) :
                         if event.key==K_UP:
-                                speed[1]=-t
-                        #if (mat<255 and mat>160) :
+                                speed[1]=-+t
                         if event.key==K_DOWN:   
-                                speed[1]=+
-                                
+                                speed[1]=+t
                         if event.key==K_RIGHT:
                                 speed[0]=+t
-                                
                         if event.key==K_LEFT:
                                  speed[0]=-t
-                                 
                         if event.key==K_SPACE:
                                 flag=1
                 if event.type==KEYUP:
@@ -138,23 +97,23 @@ while done :
                                 speed[0]=0
                         if event.key==K_LEFT:
                                 speed[0]=0
-
+       
 # Ship Control
         if sh_e==0:
-                if pos_ship[0]>-1 and pos_ship[0]+81<801:
+                if pos_ship[0]>0 and pos_ship[0]+81<800:
                         
                         pos_ship[0]+=speed[0]*sec
                 elif pos_ship[0]<0:
-                        pos_ship[0]+=2
+                        pos_ship[0]+=1
                 elif pos_ship[0]+81>800:
-                        pos_ship[0]-=2
+                        pos_ship[0]-=1
                                 
-                if pos_ship[1]>-1 and pos_ship[1]+81<601:        
+                if pos_ship[1]>0 and pos_ship[1]+81<600:        
                         pos_ship[1]+=speed[1]*sec
                 elif pos_ship[1]<0:
-                        pos_ship[1]+=2
+                        pos_ship[1]+=1
                 elif pos_ship[1]+81>600:
-                        pos_ship[1]-=2
+                        pos_ship[1]-=1
         screen.blit(background,(0,0))       
         screen.blit(debris,[x-400,30])
         screen.blit(debris,[x,30])
@@ -177,8 +136,7 @@ while done :
                [p,q]=[800,random.randint(90,510)]
                left+=1
                if left==10:
-                       done=False
-
+                        done=False
         elif(not ae==1):
                 sa=astr_s*sec
                 p-=sa
@@ -245,7 +203,7 @@ while done2:
                                 
                         if event.type==KEYDOWN:
                                 done2=False
-ser.flushInput()
+
 pygame.quit()
                                                                 
                                 
